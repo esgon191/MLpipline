ccf2b737a0fc9d97196953d8257aabf137e30765
Timur <kaftaranov.work@gmail.com>
2024-11-22 17:06:47 +0300

    Один топик создается при запуске контейнера

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,4 +1,5 @@
 docker-compose -p kursovaya up -d
 
-docker exec -it kafka-0 echo "---------------"
+docker exec -it kafka-0 ./opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic orders --partitions 3 --replication-factor 3
+
 #zsh topics.sh
