a2645b72d0140b9a50d0f7ede5cfedd57c624727
Timur <kaftaranov.work@gmail.com>
2024-11-25 10:29:10 +0300

    Топики создаются

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,5 +1,7 @@
 docker-compose -p kursovaya up -d
 
-docker exec -it kafka-0 ./opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic orders --partitions 3 --replication-factor 3
+docker exec kafka-0 ./opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic a_topic --partitions 3 --replication-factor 3
+docker exec kafka-0 ./opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic b_topic --partitions 3 --replication-factor 3
+docker exec kafka-0 ./opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic saver_topic --partitions 3 --replication-factor 3
 
 #zsh topics.sh
