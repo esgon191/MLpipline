7187821bd8837eea72289801b0805641fe987fd4
Timur <kaftaranov.work@gmail.com>
2024-11-28 21:24:09 +0300

    остановился на легкой модели, однако проблема была все же в архитектуре. Надо разобраться с requests

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,4 +1,4 @@
-docker-compose -p kursovaya up -d
+docker-compose -p kursovaya up 
 
 docker exec kafka-0 ./opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic a_topic --partitions 3 --replication-factor 3
 docker exec kafka-0 ./opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic b_topic --partitions 3 --replication-factor 3
