9cec11314bb721de43b888c5a22df3789bed2c6a
esgon191 <user0000057@gmail.com>
2024-12-05 16:45:05 +0300

    Возможно нужно переписать orm из-за неправильной обработки stream

--- crud/main.py
+++ crud/main.py
@@ -1,13 +1,20 @@
 from queries.orm import AsyncORM
-import asyncio
+import asyncio, io
+from PIL import Image
 # SyncORM.check_connection()
 
 # asyncio.run(AsyncORM.create_tables())
 # asyncio.run(AsyncORM.test_insert())
 
 async def main():
-    await AsyncORM.create_tables()
-    print(await AsyncORM.first_insert_photo(way_to_photo='../data/testdata/photo.jpg', way_to_metadata='../data/testdata/metadata.json'))
-    print(await AsyncORM.get_photo(bucket_name='bucket', object_name='1'))
+    #await AsyncORM.create_tables()
+    #print(await AsyncORM.first_insert_photo(way_to_photo='../data/testdata/photo.jpg', way_to_metadata='../data/testdata/metadata.json'))
+    response = await AsyncORM.get_photo(bucket_name='bucket', object_name='1')
+    async with response['Body'] as stream:
+            image_data = await stream.read()
+
+            # Преобразование байт в объект Image (Pillow)
+            image = Image.open(io.BytesIO(image_data))
+            image.show()
 
 asyncio.run(main())
