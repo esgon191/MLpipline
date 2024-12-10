95d70a7d338da60fc92c853c76ea07ef3df52c28
esgon191 <user0000057@gmail.com>
2024-12-10 12:29:52 +0300

    Сократил количество партиций и репликаций, ограничил ресурсы моделей в угоду производительности

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,6 +1,6 @@
 docker-compose -p kursovaya up -d
 
-docker exec kafka-0 ./opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic a_topic --partitions 3 --replication-factor 3 --config max.message.bytes=10000000
-docker exec kafka-0 ./opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic b_topic --partitions 3 --replication-factor 3 --config max.message.bytes=10000000
-docker exec kafka-0 ./opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic saver_topic --partitions 3 --replication-factor 3 --config max.message.bytes=10000000
+docker exec kafka-0 ./opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic a_topic --partitions 2 --replication-factor 2 --config max.message.bytes=10000000
+docker exec kafka-0 ./opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic b_topic --partitions 2 --replication-factor 2 --config max.message.bytes=10000000
+docker exec kafka-0 ./opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic saver_topic --partitions 2 --replication-factor 2 --config max.message.bytes=10000000
 #zsh topics.sh
